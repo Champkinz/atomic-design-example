52bf32bbbefc1d53e868772615a6f9bf555d6112
Charana Amarasekara <danu_lak@live.com>
2020-04-06 11:13:58 +0530

    task 1 / sub 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import LoginPage from "./components/page/login.page";
 import DemoNav from "./components/template/demoNavigation.screen";
 
 function App() {
-  // return <LoginPage />;
-  return <DemoNav />;
+  const [taskToggle, setTaskToggle] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      const key = event.key.toLowerCase();
+      if (key === "c") {
+        setTaskToggle(taskToggle ? false : true);
+      }
+    });
+  });
+
+  return <div>{taskToggle ? <LoginPage /> : <DemoNav />}</div>;
 }
 
 export default App;
--- src/components/template/demoNavigation.screen.tsx
+++ src/components/template/demoNavigation.screen.tsx
@@ -3,15 +3,57 @@ import { Page } from "../atoms/styles";
 
 const DemoNav: FC<DemoNavProps> = (props: DemoNavProps) => {
   const [togl, setTogle] = useState(false);
+  const [togl2, setTogle2] = useState(false);
 
   return (
     <Page>
-      <div className="bg-green-400 h-full">
+      <div className="flex flex-1 flex-row bg-green-400 h-full">
         <div
-          className={togl ? "expand" : "collapse"}
+          className={togl ? "collapse" : "expand"}
           onClick={() => setTogle(togl ? false : true)}
         >
-          lol
+          <div className="flex flex-1 flex-col items-center">
+            {!togl ? (
+              <>
+                <img
+                  alt=""
+                  src={require("../../images/appSpotrLogo2.png")}
+                  width={40}
+                  height={40}
+                />
+                <div className="hover:bg-green-400 hover:opacity-50 text-white w-full flex flex-1 flex-row justify-center py-4 mt-2 border-r-4 border-teal-500">
+                  A
+                </div>
+                <div className="hover:bg-green-400 hover:opacity-50 text-white w-full flex flex-1 flex-row justify-center py-4 ">
+                  B
+                </div>
+              </>
+            ) : null}
+            <div className="buttonToggle">
+              {!togl ? <p>{`<`}</p> : <p>{`>`}</p>}
+            </div>
+          </div>
+        </div>
+        <div
+          className={togl2 ? "expand2" : "collapse2"}
+          onClick={() => setTogle2(togl2 ? false : true)}
+        >
+          <div className="flex flex-1 flex-col items-center">
+            <div className="flex flex-1 flex-row justify-center w-full py-1 sec-nav-active">
+              <div>A</div>
+              {togl2 ? <div>-App Overview</div> : null}
+            </div>
+          </div>
+          <div
+            className="buttonToggle"
+            style={{
+              boxShadow: " 0px 0px 4px #434343",
+              backgroundColor: "white",
+              color: "black",
+            }}
+          >
+            {togl2 ? <p>{`<`}</p> : <p>{`>`}</p>}
+          </div>
         </div>
       </div>
     </Page>
